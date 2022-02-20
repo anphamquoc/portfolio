451661a19f2f8f7e9cd2b56f9b9c73829d6e99b9
FRI3NDS Ấn <19521187@gmail.com>
2022-02-20 10:59:51 +0700

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,30 @@ import Projects from "./view/Project/Projects";
 import CV from "./view/Resume/CV";
 import Blogs from "./view/Blogs/Blogs";
 import Footer from "./components/Footer/Footer";
+import { useEffect, useState } from "react";
+import Loading from "./assets/port.svg";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+  if (loading) {
+    return (
+      <img
+        src={Loading}
+        alt="loading"
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "40%",
+          translate: "(-50%, -50%)",
+        }}
+      />
+    );
+  }
   return (
     <Router>
       <NavBar />
